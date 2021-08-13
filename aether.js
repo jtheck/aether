df8416231782f6bb8202b92efc26547b32c03c21
jtheck <cb.monk@gmail.com>
2021-08-12 21:23:01 -0700

    save

--- aether.js
+++ aether.js
@@ -13,7 +13,7 @@ const Mote = function(){
 var mote = new Mote();
 
   
-Check compatibility for the browser we're running this in
+// Check compatibility for the browser we're running this in
 if ("serviceWorker" in navigator) {
     if (navigator.serviceWorker.controller) {
       console.log("sw active service worker found, no need to register");

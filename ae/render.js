80d2854f73c73b2c22e861414306a97895bb9bfb
Heck <justintheck@gmail.com>
2022-10-30 04:49:26 -0700

    working input

--- ae/render.js
+++ ae/render.js
@@ -1,4 +1,5 @@
 
+var xx;
 
 window.addEventListener('DOMContentLoaded', function() {
   var canvas = document.getElementById('canvas');
@@ -106,7 +107,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
 
 
-    var tetrahedron = BABYLON.MeshBuilder.CreatePolyhedron("tetrahedron", {type: 0, size: .01}, scene);
+    var tetrahedron = BABYLON.MeshBuilder.CreatePolyhedron("tetrahedron", {type: 0, size: .03}, scene);
     tetrahedron.position.y = 2;
     mQ = [];
     for (var ix=-10;ix<10;ix+=.75) {
@@ -151,6 +152,13 @@ window.addEventListener('DOMContentLoaded', function() {
   var xrCam;
   var xrFeatures;
 
+var axes;
+var squeeze;
+
+  xx = function(){
+    return xrCam;
+  }
+
   // scene.beforeRender(function(){
   //   gY++;
   //   for(var i=0;i<mQ.length; i++){
@@ -166,6 +174,33 @@ window.addEventListener('DOMContentLoaded', function() {
     tryXR();
 
     engine.runRenderLoop(function() {
+      
+      if (axes){
+
+        if (axes.x > 0){
+          xrCam.cameraRotation.y += .02*axes.x;
+        }
+      if (axes.x < 0){
+        xrCam.cameraRotation.y += .02*axes.x;
+      }
+      var s = .5;
+      if (axes.y > 0){
+        xrCam.position.addInPlace(xrCam.getForwardRay().direction.multiplyByFloats(s,s,s));
+      }
+      if (axes.y < 0){
+        xrCam.position.subtractInPlace(xrCam.getForwardRay().direction.multiplyByFloats(s,s,s));
+      }
+      
+    }
+    if (squeeze){
+
+      if (squeeze.pressed) {
+        // console.log("sqee");
+      }
+    }
+
+
+
       gY+=.00314;
       for(var i=0;i<mQ.length; i++){
         var tt = mQ[i];
@@ -195,8 +230,6 @@ window.addEventListener('DOMContentLoaded', function() {
       xrButton.style.display = "block";
       xrFeatures = xrHelper.featuresManager;
       xrCam = xrHelper.camera;
-      // const fm = new xrHelper.FeaturesManager(xrHelper.sessionManager);
-      // console.log(fm);
 
       const controllers = new BABYLON.WebXRInput(xrHelper.sessionManager, xrHelper.camera);
       const xrayPointer = new BABYLON.WebXRControllerPointerSelection(xrHelper.sessionManager, {
@@ -206,93 +239,51 @@ window.addEventListener('DOMContentLoaded', function() {
         xrInput: controllers,
       });
       xrayPointer.attach();
-// console.log("??")
-// console.log(controllers.controllers);
       //controller input
       controllers.onControllerAddedObservable.add((controller) => {
         
         // console.log(controller);
         controller.onMotionControllerInitObservable.add((motionController) => {
-          const ids = motionController.getComponentIds();
-              const xr_ids = motionController.getComponentIds();
-              console.log(xr_ids)
-              // 0 "xr-standard-trigger"
-              // 1 "xr-standard-squeeze"
-              // 2 "xr-standard-thumbstick"
-              // 3 "a-button"
-              // 4 "b-button"
-              // 5 "thumbrest"
-                let triggerComponent = motionController.getComponent(xr_ids[0]);//xr-standard-trigger
-                triggerComponent.onButtonStateChangedObservable.add(() => {
-                    if (triggerComponent.pressed) {
-                      console.log("boop")
-                      console.log(xrCam);
-                      // xrCam.position.x += 1;
-                      console.log(xrCam.position.x)
-                      
-                        // Box_Left_Trigger.scaling= new BABYLON.Vector3(1.2,1.2,1.2);
-                    }else{
-                        // Box_Left_Trigger.scaling= new BABYLON.Vector3(1,1,1);
-                    }
-                });
-                let squeezeComponent = motionController.getComponent(xr_ids[2]);//xr-standard-thumbstick
-                squeezeComponent.onButtonStateChangedObservable.add(() => {
-                    if (squeezeComponent.isAxes()) {
-                        // we have axes data
-                        let axes = squeezeComponent.axes;
-                      // log(axes.x)
-                      // log(axes.y)
+          const xr_ids = motionController.getComponentIds();
+          console.log(xr_ids)
+          // 0 "xr-standard-trigger"
+          // 1 "xr-standard-squeeze"
+          // 2 "xr-standard-thumbstick"
+          // 3 "a-button"
+          // 4 "b-button"
+          // 5 "thumbrest"
+          if (motionController.handness === 'right') {
 
-                      if (axes.x > 0){
-                        xrCam.position.x += 1;
+            let triggerComponent = motionController.getComponent(xr_ids[0]);//xr-standard-trigger
+            triggerComponent.onButtonStateChangedObservable.add(() => {
+              squeeze = triggerComponent;
+                if (triggerComponent.pressed) {
+                  console.log("boop")
+                }else{
+                }
+            });
+            let squeezeComponent = motionController.getComponent(xr_ids[2]);//xr-standard-thumbstick
+            squeezeComponent.onButtonStateChangedObservable.add(() => {
+                if (squeezeComponent.isAxes()) {
+                  // we have axes data
+                  axes = squeezeComponent.axes;
+                }
 
-                      }
-                      if (axes.x < 0){
-                        xrCam.position.x -= 1;
-
-                      }
-                      
-                      if (axes.y > 0){
-                        xrCam.position.y += 1;
-
-                      }
-                      if (axes.y < 0){
-                        xrCam.position.y -= 1;
-                      }
-
-                    }
-                    if (squeezeComponent.pressed) {
-                      console.log("beep")
-                        // Box_Left_Squeeze.scaling= new BABYLON.Vector3(1.2,1.2,1.2);
-                    }else{
-                        // Box_Left_Squeeze.scaling=new BABYLON.Vector3(1,1,1);
-                    }
-                });
-                let squeezeComponent2 = motionController.getComponent(xr_ids[5]);//thumbrest
-                squeezeComponent2.onButtonStateChangedObservable.add(() => {
-                    if (squeezeComponent2.pressed) {
-                      console.log("bang")
-                        // Box_Left_Squeeze.scaling= new BABYLON.Vector3(1.2,1.2,1.2);
-                    }else{
-                        // Box_Left_Squeeze.scaling=new BABYLON.Vector3(1,1,1);
-                    }
-                });
-          // let thumbstickComponent = motionController.getComponent(ids[2]);//xr-standard-thumbstick
-          // thumbstickComponent.onButtonStateChangedObservable.add(() => {
-          //     if (thumbstickComponent.pressed) {
-          //         Box_Left_ThumbStick.scaling= new BABYLON.Vector3(1.2,1.2,1.2);
-          //     }else{
-          //         Box_Left_ThumbStick.scaling=new BABYLON.Vector3(1,1,1);
-          //     }
-          // /*
-          //     let axes = thumbstickComponent.axes;
-          //     Box_Left_ThumbStick.position.x += axes.x;
-          //     Box_Left_ThumbStick.position.y += axes.y;
-          // */
-          // });
-          // thumbstickComponent.onAxisValueChangedObservable.add((axes) => {
-          //     //https://playground.babylonjs.com/#INBVUY#87
-          //     //inactivate camera rotation : not working so far
+                if (squeezeComponent.pressed) {
+                  console.log("beep")
+                }else{
+                }
+            });
+            let squeezeComponent2 = motionController.getComponent(xr_ids[5]);//thumbrest
+            squeezeComponent2.onButtonStateChangedObservable.add(() => {
+                if (squeezeComponent2.pressed) {
+                  console.log("bang")
+                    // Box_Left_Squeeze.scaling= new BABYLON.Vector3(1.2,1.2,1.2);
+                }else{
+                    // Box_Left_Squeeze.scaling=new BABYLON.Vector3(1,1,1);
+                }
+            });
+          }
 
           //     /*
           //     let rotationValue = 0;
@@ -317,172 +308,6 @@ window.addEventListener('DOMContentLoaded', function() {
           //     // console.log(values.x, values.y);
           // });
 
-            // if (motionController.handness === 'left') {
-            //     const xr_ids = motionController.getComponentIds();
-            //     let triggerComponent = motionController.getComponent(xr_ids[0]);//xr-standard-trigger
-            //     triggerComponent.onButtonStateChangedObservable.add(() => {
-            //         if (triggerComponent.pressed) {
-            //             Box_Left_Trigger.scaling= new BABYLON.Vector3(1.2,1.2,1.2);
-            //         }else{
-            //             Box_Left_Trigger.scaling= new BABYLON.Vector3(1,1,1);
-            //         }
-            //     });
-            //     let squeezeComponent = motionController.getComponent(xr_ids[1]);//xr-standard-squeeze
-            //     squeezeComponent.onButtonStateChangedObservable.add(() => {
-            //         if (squeezeComponent.pressed) {
-            //             Box_Left_Squeeze.scaling= new BABYLON.Vector3(1.2,1.2,1.2);
-            //         }else{
-            //             Box_Left_Squeeze.scaling=new BABYLON.Vector3(1,1,1);
-            //         }
-            //     });
-            //     let thumbstickComponent = motionController.getComponent(xr_ids[2]);//xr-standard-thumbstick
-            //     thumbstickComponent.onButtonStateChangedObservable.add(() => {
-            //         if (thumbstickComponent.pressed) {
-            //             Box_Left_ThumbStick.scaling= new BABYLON.Vector3(1.2,1.2,1.2);
-            //         }else{
-            //             Box_Left_ThumbStick.scaling=new BABYLON.Vector3(1,1,1);
-            //         }
-            //     /*
-            //         let axes = thumbstickComponent.axes;
-            //         Box_Left_ThumbStick.position.x += axes.x;
-            //         Box_Left_ThumbStick.position.y += axes.y;
-            //     */
-            //     });
-            //     thumbstickComponent.onAxisValueChangedObservable.add((axes) => {
-            //         //https://playground.babylonjs.com/#INBVUY#87
-            //         //inactivate camera rotation : not working so far
-
-            //         /*
-            //         let rotationValue = 0;
-            //         const matrix = new BABYLON.Matrix();
-            //         let deviceRotationQuaternion = webXRInput.xrCamera.getDirection(BABYLON.Axis.Z).toQuaternion(); // webXRInput.xrCamera.rotationQuaternion;
-            //         var angle = rotationValue * (Math.PI / 8);
-            //         var quaternion = BABYLON.Quaternion.RotationAxis(BABYLON.Axis.Y, angle);
-            //         const move = new BABYLON.Vector3(0,0,0);
-            //         deviceRotationQuaternion = deviceRotationQuaternion.multiply(quaternion);
-            //         BABYLON.Matrix.FromQuaternionToRef(deviceRotationQuaternion, matrix);
-            //         const addPos = BABYLON.Vector3.TransformCoordinates(move, matrix);
-            //         addPos.y = 0;
-
-            //         webXRInput.xrCamera.position = webXRInput.xrCamera.position.add(addPos);
-            //         // webXRInput.xrCamera.rotationQuaternion = BABYLON.Quaternion.Identity();
-                    
-            //         //webXRInput.xrCamera.rotation = new BABYLON.Vector3(0,0,0);
-            //         */
-            //         //Box_Left_ThumbStick is moving according to stick axes but camera rotation is also changing..
-            //         // Box_Left_ThumbStick.position.x += (axes.x)/100;
-            //       //  Box_Left_ThumbStick.position.y -= (axes.y)/100;
-            //         // console.log(values.x, values.y);
-            //     });
-
-            //     let xbuttonComponent = motionController.getComponent(xr_ids[3]);//x-button
-            //     xbuttonComponent.onButtonStateChangedObservable.add(() => {
-            //         if (xbuttonComponent.pressed) {
-            //             Sphere_Left_XButton.scaling= new BABYLON.Vector3(1.2,1.2,1.2);
-                        
-            //         }else{
-            //             Sphere_Left_XButton.scaling=new BABYLON.Vector3(1,1,1);  
-            //         }
-            //     });
-            //     let ybuttonComponent = motionController.getComponent(xr_ids[4]);//y-button
-            //     ybuttonComponent.onButtonStateChangedObservable.add(() => {
-            //         if (ybuttonComponent.pressed) {
-            //             Sphere_Left_YButton.scaling= new BABYLON.Vector3(1.2,1.2,1.2);
-                        
-            //         }else{
-            //             Sphere_Left_YButton.scaling=new BABYLON.Vector3(1,1,1);  
-            //         }
-            //     });
-            //     /* not worked.
-            //     let thumbrestComponent = motionController.getComponent(xr_ids[5]);//thumrest
-            //     thumbrestComponent.onButtonStateChangedObservable.add(() => {
-            //         //not worked
-            //         if ((thumbrestComponent.value>0.1&&thumbrestComponent.value<0.6) {
-            //             sphere1.position.y=10;
-            //         }
-            //         if(thumbrestComponent.touched){
-            //               sphere1.position.y=10;
-            //         }
-
-            //     });  
-            //     */              
-            // }
-            // if (motionController.handness === 'right') {
-            //     const xr_ids = motionController.getComponentIds();
-            //     let triggerComponent = motionController.getComponent(xr_ids[0]);//xr-standard-trigger
-            //     triggerComponent.onButtonStateChangedObservable.add(() => {
-            //         if (triggerComponent.pressed) {
-            //             Box_Right_Trigger.scaling= new BABYLON.Vector3(1.2,1.2,1.2);
-                    
-            //         }else{
-            //             Box_Right_Trigger.scaling= new BABYLON.Vector3(1,1,1);
-                    
-            //         }
-            //     });
-            //     let squeezeComponent = motionController.getComponent(xr_ids[1]);//xr-standard-squeeze
-            //     squeezeComponent.onButtonStateChangedObservable.add(() => {
-            //         if (squeezeComponent.pressed) {
-            //             Box_Right_Squeeze.scaling= new BABYLON.Vector3(1.2,1.2,1.2);
-                      
-            //         }else{
-            //             Box_Right_Squeeze.scaling=new BABYLON.Vector3(1,1,1);
-            //         }
-            //     });
-            //     let thumbstickComponent = motionController.getComponent(xr_ids[2]);//xr-standard-thumbstick
-            //     thumbstickComponent.onButtonStateChangedObservable.add(() => {
-            //         if (thumbstickComponent.pressed) {
-            //             Box_Right_ThumbStick.scaling= new BABYLON.Vector3(1.2,1.2,1.2);
-            //         }else{
-            //             Box_Right_ThumbStick.scaling=new BABYLON.Vector3(1,1,1);
-            //         }
-
-            //     });
-            //     thumbstickComponent.onAxisValueChangedObservable.add((axes) => {
-            //         //Box_Right_ThumbStick is moving according to stick axes but camera rotation is also changing..
-            //         // Box_Right_ThumbStick.position.x += (axes.x)/100;
-            //         // Box_Right_ThumbStick.position.y += (axes.y)/100;
-            //         // console.log(values.x, values.y);
-            //     });
-
-            //     let abuttonComponent = motionController.getComponent(xr_ids[3]);//a-button
-            //     abuttonComponent.onButtonStateChangedObservable.add(() => {
-            //         if (abuttonComponent.pressed) {
-            //             Sphere_Right_AButton.scaling= new BABYLON.Vector3(1.2,1.2,1.2);
-            //         }else{
-            //             Sphere_Right_AButton.scaling=new BABYLON.Vector3(1,1,1);  
-            //         }
-            //     });
-            //     let bbuttonComponent = motionController.getComponent(xr_ids[4]);//b-button
-            //     bbuttonComponent.onButtonStateChangedObservable.add(() => {
-            //         if (bbuttonComponent.pressed) {
-            //             Sphere_Right_BButton.scaling= new BABYLON.Vector3(1.2,1.2,1.2);
-                        
-            //         }else{
-            //             Sphere_Right_BButton.scaling=new BABYLON.Vector3(1,1,1);  
-            //         }
-            //     });
-            //     /* not worked.
-            //     let thumbrestComponent = motionController.getComponent(xr_ids[5]);//thumrest
-            //     thumbrestComponent.onButtonStateChangedObservable.add(() => {
-            //         //not worked
-            //         if ((thumbrestComponent.value>0.1&&thumbrestComponent.value<0.6) {
-            //             sphere1.position.y=10;
-            //         }
-            //         if(thumbrestComponent.touched){
-            //               sphere1.position.y=10;
-            //         }
-
-            //     });  
-            //     */              
-
-            //     /*
-            //     const xr_ids = motionController.getComponentIds();
-            //     for (let i=0;i<xr_ids.length;i++){
-            //         console.log("right:"+xr_ids[i]);
-            //     }
-            //     */
-            // }
-
         })
 
     });
@@ -490,15 +315,8 @@ window.addEventListener('DOMContentLoaded', function() {
 
 
 
-
-
-
-
-
-
-
   
-      console.log(controllers);
+      // console.log(controllers);
       controllers.onControllerAddedObservable.add((controller) => {
 
         console.log(controller);
